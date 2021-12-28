89f8e2ec8f09bf4d4c60e85667c76b45304c84f3
M OdhieP <tiotio179@gmail.com>
2021-12-29 00:16:59 +0700

    modified:   index.php add paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Test remote git</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quaerat officia, minus, excepturi repudiandae, laudantium impedit voluptas sit illo voluptate beatae aut cupiditate autem ratione laboriosam nemo exercitationem eius ipsam.</p>
 </body>
 </html>
